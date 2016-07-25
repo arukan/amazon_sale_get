cf4bba4c5317fa649f980a2f59fb323e95812537
mryoshio <yoshiokaas@gmail.com>
2016-07-25 14:26:50 +0900

    refactoring
    - add Gemfile, dotenv
    - add gitignore
    - add README
    - refactor code

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Setup
+
+$ cp -iv .env.sample .env
+
+$ vi .env # update for your environment
+
+$ bundle
+
+## Usage
+
+$ ruby sales_info.rb
--- item_search.rb
+++ item_search.rb
@@ -1,54 +1,33 @@
 #!/usr/bin/env ruby
 
-require 'time'
-require 'uri'
-require 'openssl'
-require 'base64'
-
-=begin
-# Your AWS Access Key ID, as taken from the AWS Your Account page
-AWS_ACCESS_KEY_ID = "AKIAJ5INMXIBT3ZRZHIA"
-
-# Your AWS Secret Key corresponding to the above ID, as taken from the AWS Your Account page
-AWS_SECRET_KEY = "cD7GVBnNO90fHDwxqja4cSM/WTrfHPcKdgxWh8TA"
-
-# The region you are interested in
-ENDPOINT = "webservices.amazon.co.jp"
-
-REQUEST_URI = "/onca/xml"
-
-=end
-
-def itemSearch(browseNodeId,pageNumber)
-
-params = {
-    "Service" => "AWSECommerceService",
-    "Operation" => "ItemSearch",
-    "AWSAccessKeyId" => "XXXXXXXX",
-    "AssociateTag" => "XXX-XXX-XXX",
-    "SearchIndex" => "Books",
-    "ResponseGroup" => "Images,ItemAttributes,Offers,SalesRank",
-    "Sort" => "salesrank",
-    "BrowseNode" => browseNodeId,
-    "ItemPage" => pageNumber
+def itemSearch(browseNodeId, pageNumber)
+  params = {
+    'Service' => 'AWSECommerceService',
+    'Operation' => 'ItemSearch',
+    'AWSAccessKeyId' => ENV['AWS_ACCESS_KEY_ID'],
+    'AssociateTag' => ENV['ASSOCIATE_TAG'],
+    'SearchIndex' => 'Books',
+    'ResponseGroup' => 'Images,ItemAttributes,Offers,SalesRank',
+    'Sort' => 'salesrank',
+    'BrowseNode' => browseNodeId,
+    'ItemPage' => pageNumber
     
-}
+  }
 
-# Set current timestamp if not set
-params["Timestamp"] = Time.now.gmtime.iso8601 if !params.key?("Timestamp")
+  # Set current timestamp if not set
+  params['Timestamp'] = Time.now.gmtime.iso8601 if !params.key?('Timestamp')
 
-# Generate the canonical query
-canonical_query_string = params.sort.collect do |key, value|
+  # Generate the canonical query
+  canonical_query_string = params.sort.collect do |key, value|
     [URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")), URI.escape(value.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))].join('=')
-end.join('&')
+  end.join('&')
 
-# Generate the string to be signed
-string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
+  # Generate the string to be signed
+  string_to_sign = "GET\n#{ENDPOINT}\n#{REQUEST_URI}\n#{canonical_query_string}"
 
-# Generate the signature required by the Product Advertising API
-signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), AWS_SECRET_KEY, string_to_sign)).strip()
+  # Generate the signature required by the Product Advertising API
+  signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), ENV['AWS_SECRET_KEY'], string_to_sign)).strip()
 
-# Generate the signed URL
-"http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
-
+  # Generate the signed URL
+  "http://#{ENDPOINT}#{REQUEST_URI}?#{canonical_query_string}&Signature=#{URI.escape(signature, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}"
 end
